8c47ddc86e6ce5b80ca50450b119b469a4578c88
Wikemon <panovartem2008@gmail.com>
2025-08-03 12:47:38 +0300

    Update main.go

--- main/main.go
+++ main/main.go
@@ -8,9 +8,10 @@ import (
 func main() {
 	// Получаем локальный IP
 	fmt.Printf("Starting P2P chat. Your IP: %s\n", internal.GetLocalIP())
+	fmt.Printf("BroadcastAddr: %s\n", internal.GetBroadcastAddr())
 
 	// Запускаем сервер для приема чат-сообщений
-	go internal.StartChatServer()
+	//go internal.StartChatServer()
 
 	// Запускаем broadcast сервер для обнаружения пиров
 	go internal.StartBroadcastServer()
@@ -18,6 +19,8 @@ func main() {
 	// Запускаем broadcast клиент для оповещения о себе
 	go internal.StartBroadcastClient()
 
-	// Запускаем интерфейс пользователя
-	internal.StartUserInterface()
+	for {
+		fmt.Scan()
+	}
+	//internal.StartUserInterface()
 }
